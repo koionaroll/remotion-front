ac77e5cef8e3b15f3e00c553b42bf640f9307dfd
Khoi Tran <tranvankhoi2002@gmail.com>
2023-04-26 11:46:56 -0700

    added tailwind to program

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.scss'
+import './index.css'
 
 function App() {
 
   return (
-    <div>asfsd</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
