662f7f40092886d7beccda1a400c9c153ed9064e
Khoi Tran <tranvankhoi2002@gmail.com>
2023-04-29 13:09:13 -0700

    added dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './index.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-4xl font-bold underline">
+      Hello world
     </h1>
   )
 }
